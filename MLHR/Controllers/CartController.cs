8bb1f414f38141dee02e535b20f0148e109cbc7a
ho trung hieu <trunghieuho0909@gmail.com>
2025-01-15 11:31:43 +0700

    dua san pham co san vao gio hang va tinh gia

---
+++ MLHR/Controllers/CartController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessObject;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace MLHR.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly CartService _cartService;
+
+        public CartController()
+        {
+            _cartService = new CartService();
+        }
+
+        [HttpGet("available-products")]
+        public IActionResult GetAvailableProducts()
+        {
+            var products = _cartService.GetAvailableProducts();
+            return Ok(products);
+        }
+
+        [HttpPost("add-from-stock")]
+        public IActionResult AddProductToCart([FromQuery] string productId, [FromQuery] int quantity)
+        {
+            var result = _cartService.AddProductToCart(productId, quantity);
+            return Ok(new { Message = result });
+        }
+
+        [HttpGet("cart")]
+        public IActionResult GetCartItems()
+        {
+            var cartItems = _cartService.GetCartItems();
+            var totalPrice = _cartService.GetCartTotalPrice();
+
+            return Ok(new
+            {
+                Items = cartItems,
+                TotalPrice = totalPrice
+            });
+        }
+    }
+}
