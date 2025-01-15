8420487971e39a3b14b6d60f0ee615fe9de241db
ho trung hieu <trunghieuho0909@gmail.com>
2025-01-15 20:31:16 +0700

    test2

--- MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
+++ MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb1f414f38141dee02e535b20f0148e109cbc7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a46bbf682a40058e1cbd3f39f679073cdbb2d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
