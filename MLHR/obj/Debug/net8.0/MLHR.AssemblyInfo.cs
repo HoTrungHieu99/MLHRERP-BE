d9841986eccb5bda543f5779e310770617ea99e6
ho trung hieu <trunghieuho0909@gmail.com>
2025-01-16 23:30:39 +0700

    change irgone

--- MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
+++ MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a46bbf682a40058e1cbd3f39f679073cdbb2d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71433e5e718fbb3aa9978b3ac61ab05a2130694")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
