df99c7a558cf606f318b309bff2fd6878c5d9bb4
caokienquoc1582002@gmail.com <caokienquoc1582002@gmail.com>
2025-03-25 03:39:54 +0700

    update source

--- BusinessObject/DTO/PaymentDTO/CreatePaymentRequest.cs
+++ BusinessObject/DTO/PaymentDTO/CreatePaymentRequest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BusinessObject.DTO.PaymentDTO
@@ -11,6 +12,7 @@ namespace BusinessObject.DTO.PaymentDTO
         public Guid? OrderId { get; set; }
         public long AgencyId { get; set; }
         public decimal Price { get; set; }
+        [JsonIgnore]
         public string? Description { get; set; }
 
     }
