47a5c7e3627e617984cdf2af8fb789ad17013358
caokienquoc1582002@gmail.com <caokienquoc1582002@gmail.com>
2025-04-09 01:07:57 +0700

    update source

--- MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
+++ MLHR/obj/Debug/net8.0/MLHR.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5073f539a291c576bbd7c74158c61fdfe012458a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c23dc3e6b5e6ac05841a1d0abcf5f22a0d302a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLHR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/Service/PdfService.cs
+++ Services/Service/PdfService.cs
@@ -18,7 +18,7 @@ namespace Services.Service
         private readonly IConverter _converter;
         private static bool _libraryLoaded = false;
 
-        public PdfService(IConverter converter)
+        /*public PdfService(IConverter converter)
         {
             // ✅ Load thư viện native nếu chạy trên Linux và chưa load lần nào
             if (!_libraryLoaded && RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
@@ -38,7 +38,29 @@ namespace Services.Service
             }
 
             _converter = converter;
-        }
+        }*/
+
+        /*public byte[] GeneratePdf(string htmlContent)
+        {
+            var doc = new HtmlToPdfDocument
+            {
+                GlobalSettings = new GlobalSettings
+                {
+                    PaperSize = PaperKind.A4,
+                    Orientation = Orientation.Portrait,
+                    DocumentTitle = "Phiếu Nhập Kho"
+                },
+                Objects = {
+                new ObjectSettings
+                {
+                    HtmlContent = htmlContent,
+                    WebSettings = { DefaultEncoding = "utf-8" }
+                }
+            }
+            };
+
+            return _converter.Convert(doc);
+        }*/
 
         public byte[] GeneratePdf(string htmlContent)
         {
